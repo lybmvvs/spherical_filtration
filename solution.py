214666609ab753851030afe6a1ecaf3c376434fd
lybmvvs <stik9543@gmail.com>
2021-11-27 13:25:45 +0300

    mistake clear: summarize space between units

--- solution.py
+++ solution.py
@@ -684,8 +684,7 @@ class Inpxlsx():
 
     def exp_file(self):
         global final_data
-        final_data = final_data.drop(['Unnamed: 0'],
-                                     axis=1)
+
         final_data.to_excel('solution.xlsx',index=False)
 
     ui.pushButton_3.clicked.connect(exp_file)
